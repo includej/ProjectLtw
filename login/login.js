0c30e673e3bc6487f6b99dccb5d8d650f0b308f5
Marco-47 <63449491+Marco-47@users.noreply.github.com>
2020-05-11 21:58:33 +0200

    v1.0.3
    Aggiunta pagina password dimenticate e
    relativo codice js per validare mail. Aggiunto nella pagine del login il link a password dimenticata.

--- login/login.js
+++ login/login.js
@@ -27,8 +27,15 @@ $(document).ready(function(){
   });
   $("#login").submit(function() {   //event onSubmit form #login
     if (!check()){
-      document.getElementById("error").style.display = "inline";
+        $(".error-login").css("display","inline");
         return false;       
     };
+  });
+  $("#dimenticata").submit(() => {
+      if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test("#dimenticata")){   //event onSubmit form #dimenticata
+         return true;
+      };
+        $(".error-mail").css("display","inline");
+         return false;
   });
 });
